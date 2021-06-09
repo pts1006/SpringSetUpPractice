084b7bcfced8b2b1cbb29b8bb6f6dad1994a25c3
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-06-09 22:10:58 +0900

    Add files via upload

---
+++ springHello/src/main/java/co/jjw/springHello/HomeController.java
@@ -0,0 +1,21 @@
+package co.jjw.springHello;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home.do")
+	public String home(Locale locale, Model model) {
+		
+		return "home";
+	}
+	
+}
